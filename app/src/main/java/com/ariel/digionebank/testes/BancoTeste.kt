61c4b7bbb9c7e5ed62302b96de4aafc4bfe445d4
ariel96viana <ariel96viana@gmail.com>
2022-06-10 14:29:20 -0300

    Criada Data Class Banco, val nome e numero.

---
+++ app/src/main/java/com/ariel/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,11 @@
+package com.ariel.digionebank.testes
+
+import com.ariel.digionebank.Banco
+
+fun main(){
+    val digiOneBank = Banco("Digi One", 13)
+
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+}
