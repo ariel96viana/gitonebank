4b070f5cea52491a6590a15aa10c75ce48c86c8f
ariel96viana <ariel96viana@gmail.com>
2022-06-14 18:20:43 -0300

    Abstraindo as classes para maior precisão das suas utilidades.

--- app/src/main/java/com/ariel/digionebank/Pessoa.kt
+++ app/src/main/java/com/ariel/digionebank/Pessoa.kt
@@ -1,15 +1,6 @@
 package com.ariel.digionebank
 
-class Pessoa {
-
-    var nome: String = "Ariel Viana Oliveira"
-
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-
-
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
